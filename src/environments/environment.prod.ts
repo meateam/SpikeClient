981b275ab5404a3e9d06eb427b01903f57bb6092
misha1235000 <misha1235000@gmail.com>
2020-11-11 12:17:28 +0200

    chore: Fix spaces design

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  production: true,
-  environmentName: 'PROD',
-  SERVER_HOST: 'https://localhost',
-  SERVER_PORT: '3005',
-  CLIENT_PORT: '4200',
-  PRIVATE_KEY_PATH: 'src/certs/files/privatekey.pem',
-  CERT_PATH: 'src/certs/files/certificate.pem',
-  ELASTIC_APM_SERVICE_NAME: 'apm_service',
-  ELASTIC_APM_SERVER_URL: 'apm_server',
-  ELASTIC_APM_SECRET_TOKEN: 'apm_secret',
-  ELASTIC_APM_ACTIVE: 'false',
-  SHRAGA_URL: 'http://13.79.7.3'
+    production: true,
+    environmentName: 'PROD',
+    SERVER_HOST: 'https://localhost',
+    SERVER_PORT: '3005',
+    CLIENT_PORT: '4200',
+    PRIVATE_KEY_PATH: 'src/certs/files/privatekey.pem',
+    CERT_PATH: 'src/certs/files/certificate.pem',
+    ELASTIC_APM_SERVICE_NAME: 'apm_service',
+    ELASTIC_APM_SERVER_URL: 'apm_server',
+    ELASTIC_APM_SECRET_TOKEN: 'apm_secret',
+    ELASTIC_APM_ACTIVE: 'false',
+    SHRAGA_URL: 'http://13.79.7.3'
 };
